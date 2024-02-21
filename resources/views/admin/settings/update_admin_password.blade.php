3963eeb9a095c44587c3b2c34b9385554e1dfa0e
hernandomanotoa <hernandomanotoa@hotmail.com>
2024-02-20 22:48:29 -0500

    Traduccion formularios administrador

--- resources/views/admin/settings/update_admin_password.blade.php
+++ resources/views/admin/settings/update_admin_password.blade.php
@@ -8,13 +8,15 @@
                 <div class="col-md-12 grid-margin">
                     <div class="row">
                         <div class="col-12 col-xl-8 mb-4 mb-xl-0">
-                            <h3 class="font-weight-bold">Admin Settings</h3>
+                            <h3 class="font-weight-bold">{{ __('Admin Settings') }}</h3>
                         </div>
                         <div class="col-12 col-xl-4">
                             <div class="justify-content-end d-flex">
                                 <div class="dropdown flex-md-grow-1 flex-xl-grow-0">
-                                    <button class="btn btn-sm btn-light bg-white dropdown-toggle" type="button" id="dropdownMenuDate2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-                                    <i class="mdi mdi-calendar"></i> Today (10 Jan 2021)
+                                    <button class="btn btn-sm btn-light bg-white dropdown-toggle" type="button"
+                                        id="dropdownMenuDate2" data-toggle="dropdown" aria-haspopup="true"
+                                        aria-expanded="true">
+                                        <i class="mdi mdi-calendar"></i> Today (10 Jan 2021)
                                     </button>
                                     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuDate2">
                                         <a class="dropdown-item" href="#">January - March</a>
@@ -36,13 +38,14 @@
 
                             {{-- Our Bootstrap error code in case of wrong current password or the new password and confirm password are not matching: --}}
                             {{-- Determining If An Item Exists In The Session (using has() method): https://laravel.com/docs/9.x/session#determining-if-an-item-exists-in-the-session --}}
-                            @if (Session::has('error_message')) <!-- Check AdminController.php, updateAdminPassword() method -->
-                            <div class="alert alert-danger alert-dismissible fade show" role="alert">
-                                <strong>Error:</strong> {{ Session::get('error_message') }}
-                                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                                </button>
-                            </div>
+                            @if (Session::has('error_message'))
+                                <!-- Check AdminController.php, updateAdminPassword() method -->
+                                <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                                    <strong>Error:</strong> {{ Session::get('error_message') }}
+                                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
                             @endif
 
 
@@ -50,18 +53,19 @@
                             {{-- Displaying The Validation Errors: https://laravel.com/docs/9.x/validation#quick-displaying-the-validation-errors AND https://laravel.com/docs/9.x/blade#validation-errors --}}
                             {{-- Determining If An Item Exists In The Session (using has() method): https://laravel.com/docs/9.x/session#determining-if-an-item-exists-in-the-session --}}
                             {{-- Our Bootstrap success message in case of updating admin password is successful: --}}
-                            @if (Session::has('success_message')) <!-- Check AdminController.php, updateAdminPassword() method -->
+                            @if (Session::has('success_message'))
+                                <!-- Check AdminController.php, updateAdminPassword() method -->
                                 <div class="alert alert-success alert-dismissible fade show" role="alert">
-                                    <strong>Success:</strong> {{ Session::get('success_message') }}
+                                    <strong>{{ __('Success') }}:</strong> {{ Session::get('success_message') }}
                                     <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                                    <span aria-hidden="true">&times;</span>
+                                        <span aria-hidden="true">&times;</span>
                                     </button>
                                 </div>
                             @endif
 
 
 
-                            <h4 class="card-title">Update Admin Password</h4>
+                            <h4 class="card-title">{{ __('Update Admin Password') }}</h4>
 
 
 
@@ -70,28 +74,33 @@
 
 
                                 <div class="form-group">
-                                    <label>Admin Username/Email</label>
-                                    <input class="form-control" value="{{ $adminDetails['email'] }}" readonly> <!-- Check updateAdminPassword() method in AdminController.php -->
+                                    <label>{{ __('Admin Username/Email') }}</label>
+                                    <input class="form-control" value="{{ $adminDetails['email'] }}" readonly>
+                                    <!-- Check updateAdminPassword() method in AdminController.php -->
                                 </div>
                                 <div class="form-group">
-                                    <label>Admin Type</label>
+                                    <label>{{ __('Admin Type') }}</label>
                                     <input class="form-control" value="{{ $adminDetails['type'] }}" readonly>
                                 </div>
                                 <div class="form-group">
-                                    <label for="current_password">Current Password</label>
-                                    <input type="password" class="form-control" id="current_password" placeholder="Enter Current Password" name="current_password" required>
-                                    <span id="check_password"></span> <!-- We'll use it in the AJAX call in admin/js/custom.js to show if the password is correct or not -->
+                                    <label for="current_password">{{ __('Current Password') }}</label>
+                                    <input type="password" class="form-control" id="current_password"
+                                        placeholder="Enter Current Password" name="current_password" required>
+                                    <span id="check_password"></span>
+                                    <!-- We'll use it in the AJAX call in admin/js/custom.js to show if the password is correct or not -->
                                 </div>
                                 <div class="form-group">
-                                    <label for="new_password">New Password</label>
-                                    <input type="password" class="form-control" id="new_password" placeholder="Enter New Password" name="new_password" required>
+                                    <label for="new_password">{{ __('New Password') }}</label>
+                                    <input type="password" class="form-control" id="new_password"
+                                        placeholder="Enter New Password" name="new_password" required>
                                 </div>
                                 <div class="form-group">
-                                    <label for="confirm_password">Confirm Password</label>
-                                    <input type="password" class="form-control" id="confirm_password" placeholder="Confirm Password" name="confirm_password" required>
+                                    <label for="confirm_password">{{ __('Confirm Password') }}</label>
+                                    <input type="password" class="form-control" id="confirm_password"
+                                        placeholder="Confirm Password" name="confirm_password" required>
                                 </div>
-                                <button type="submit" class="btn btn-primary mr-2">Submit</button>
-                                <button type="reset"  class="btn btn-light">Cancel</button>
+                                <button type="submit" class="btn btn-primary mr-2">{{ __('Submit') }}</button>
+                                <button type="reset" class="btn btn-light">{{ __('Cancel') }}</button>
                             </form>
 
 
