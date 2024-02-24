2d124ccbf6b98cdb4c383f42783163c32771133a
Hernando Manotoa <hernandomanotoa@hotmail.com>
2024-02-23 23:16:52 -0500

    Actualizacion

--- database/seeders/AdminsTableSeeder.php
+++ database/seeders/AdminsTableSeeder.php
@@ -22,8 +22,7 @@ class AdminsTableSeeder extends Seeder
         // Note: Check DatabaseSeeder.php
         $adminRecords = [
             [
-                'id'        => 1,
-                'name'      => 'Ahmed Yahya',
+                'name'      => 'Administrador',
                 'type'      => 'superadmin',
                 'vendor_id' => 0, // `vendor_id` is zero 0 because 'type' is not 'vendor' (it's actually 'superadmin')
                 'mobile'    => '9800000000',
@@ -35,12 +34,42 @@ class AdminsTableSeeder extends Seeder
 
             
             [
-                'id'        => 2,
-                'name'      => 'John Singh - Vendor',
+                'name'      => 'Vendedor',
                 'type'      => 'vendor',
                 'vendor_id' => 1, // `vendor_id` is one 1 because 'type' is 'vendor'
                 'mobile'    => '9700000000',
                 'email'     => 'john@admin.com',
+                'password'  => '$2a$12$xvkjSScUPRexfcJTAy9ATutIeGUuRgJrjDIdL/.xlrddEvRZINpeC', // This is the encryption of '123456'    // using https://bcrypt-generator.com/
+                'image'     => '',
+                'status'    => 1, // Our authentication logic in the login() method in the AdminController won't allow this admin logging in in case of 'status' = 0
+            ],
+
+            [
+                'name'      => 'Hernando',
+                'type'      => 'superadmin',
+                'vendor_id' => 0, // `vendor_id` is one 1 because 'type' is 'vendor'
+                'mobile'    => '9700000000',
+                'email'     => 'hernando@admin.com',
+                'password'  => '$2a$12$xvkjSScUPRexfcJTAy9ATutIeGUuRgJrjDIdL/.xlrddEvRZINpeC', // This is the encryption of '123456'    // using https://bcrypt-generator.com/
+                'image'     => '',
+                'status'    => 1, // Our authentication logic in the login() method in the AdminController won't allow this admin logging in in case of 'status' = 0
+            ],
+            [
+                'name'      => 'Raul',
+                'type'      => 'superadmin',
+                'vendor_id' => 0, // `vendor_id` is one 1 because 'type' is 'vendor'
+                'mobile'    => '9700000000',
+                'email'     => 'raul@admin.com',
+                'password'  => '$2a$12$xvkjSScUPRexfcJTAy9ATutIeGUuRgJrjDIdL/.xlrddEvRZINpeC', // This is the encryption of '123456'    // using https://bcrypt-generator.com/
+                'image'     => '',
+                'status'    => 1, // Our authentication logic in the login() method in the AdminController won't allow this admin logging in in case of 'status' = 0
+            ],
+            [
+                'name'      => 'Mauricio',
+                'type'      => 'superadmin',
+                'vendor_id' => 0, // `vendor_id` is one 1 because 'type' is 'vendor'
+                'mobile'    => '9700000000',
+                'email'     => 'mauricio@admin.com',
                 'password'  => '$2a$12$xvkjSScUPRexfcJTAy9ATutIeGUuRgJrjDIdL/.xlrddEvRZINpeC', // This is the encryption of '123456'    // using https://bcrypt-generator.com/
                 'image'     => '',
                 'status'    => 1, // Our authentication logic in the login() method in the AdminController won't allow this admin logging in in case of 'status' = 0
