ea77096541a989f5357e5cbda350668623389fed
Hernando Manotoa <hernandomanotoa@hotmail.com>
2024-02-27 16:30:21 -0500

    Creacion de usuarios y categorias automaticas

--- database/seeders/CategoriesTableSeeder.php
+++ database/seeders/CategoriesTableSeeder.php
@@ -61,6 +61,76 @@ class CategoriesTableSeeder extends Seeder
                 'meta_keywords'     => '',
                 'status'            => 1,
             ],
+            [
+                'id'                => 4,
+                'parent_id'         => 0, // 0 because like Men, Women categories that don't have a prent category
+                'section_id'        => 1, // 1 is the parent 'Cloting' section
+                'category_name'     => 'Kid2',
+                'category_image'    => '',
+                'category_discount' => 0,
+                'description'       => '',
+                'url'               => 'kids2',
+                'meta_title'        => '',
+                'meta_description'  => '',
+                'meta_keywords'     => '',
+                'status'            => 1,
+            ],
+            [
+                'id'                => 5,
+                'parent_id'         => 0, // 0 because like Men, Women categories that don't have a prent category
+                'section_id'        => 1, // 1 is the parent 'Cloting' section
+                'category_name'     => 'Kids3',
+                'category_image'    => '',
+                'category_discount' => 0,
+                'description'       => '',
+                'url'               => 'kids3',
+                'meta_title'        => '',
+                'meta_description'  => '',
+                'meta_keywords'     => '',
+                'status'            => 1,
+            ],
+            [
+                'id'                => 6,
+                'parent_id'         => 0, // 0 because like Men, Women categories that don't have a prent category
+                'section_id'        => 1, // 1 is the parent 'Cloting' section
+                'category_name'     => 'Kids4',
+                'category_image'    => '',
+                'category_discount' => 0,
+                'description'       => '',
+                'url'               => 'kids4',
+                'meta_title'        => '',
+                'meta_description'  => '',
+                'meta_keywords'     => '',
+                'status'            => 1,
+            ],
+            [
+                'id'                => 7,
+                'parent_id'         => 0, // 0 because like Men, Women categories that don't have a prent category
+                'section_id'        => 1, // 1 is the parent 'Cloting' section
+                'category_name'     => 'Kids5',
+                'category_image'    => '',
+                'category_discount' => 0,
+                'description'       => '',
+                'url'               => 'kids5',
+                'meta_title'        => '',
+                'meta_description'  => '',
+                'meta_keywords'     => '',
+                'status'            => 1,
+            ],
+            [
+                'id'                => 8,
+                'parent_id'         => 0, // 0 because like Men, Women categories that don't have a prent category
+                'section_id'        => 1, // 1 is the parent 'Cloting' section
+                'category_name'     => 'Categoria8',
+                'category_image'    => '',
+                'category_discount' => 0,
+                'description'       => '',
+                'url'               => 'Categoria8',
+                'meta_title'        => '',
+                'meta_description'  => '',
+                'meta_keywords'     => '',
+                'status'            => 1,
+            ],
         ];
 
         // Note: Check DatabaseSeeder.php
